72bd0f21ceef84480e9253c510ccaf61407bf540
charisse10 <1995510@edu.vaniercollege.qc.ca>
2023-10-01 11:26:28 -0400

    added header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,18 @@ export default function Navbar() {
     return (
         <div>
             <nav>
-            <a href="/">HOME</a> &nbsp;
-            <a href="/user">USER</a> &nbsp;
-                <a href="/admin">ADMIN</a>
+                <div class="container">
+                    <div class="header-flex">
+                        <div class="logo">
+                            <img src="/dot.png" alt="logo" />
+                            <a href="/">com / events</a>
+                        </div>
+                        <div class="access">
+                            <a href="/user">USER</a>
+                            <a href="/admin">ADMIN</a>
+                        </div>
+                    </div>
+                </div>
             </nav>
         </div>
     )
